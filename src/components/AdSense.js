df76d2fe5f818e57fc71890717eeda2cda11309f
Mohameddaif123 <154259489+Mohameddaif123@users.noreply.github.com>
2024-06-02 13:46:44 +0300

    Update AdSense.js

--- src/components/AdSense.js
+++ src/components/AdSense.js
@@ -1,36 +1,36 @@
+// AdSense.js
+
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 
-const AdSense = ({ client, slot, format = 'auto', responsive = 'true' }) => {
+const AdSense = ({ client, slot }) => {
   useEffect(() => {
-    if (window) {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    }
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
   }, []);
 
   return (
-    <div>
-      <Helmet>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-      </Helmet>
+    <Helmet>
+      <script
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${client}`}
+        crossOrigin="anonymous"
+      ></script>
       <ins
         className="adsbygoogle"
         style={{ display: 'block' }}
         data-ad-client={client}
         data-ad-slot={slot}
-        data-ad-format={format}
-        data-full-width-responsive={responsive}
+        data-ad-format="auto"
+        data-full-width-responsive="true"
       ></ins>
-    </div>
+    </Helmet>
   );
 };
 
 AdSense.propTypes = {
   client: PropTypes.string.isRequired,
   slot: PropTypes.string.isRequired,
-  format: PropTypes.string,
-  responsive: PropTypes.string,
 };
 
 export default AdSense;
