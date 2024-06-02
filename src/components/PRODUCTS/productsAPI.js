4ae92c89c7414a9db38fbf929a02770ef6ad7122
Mohameddaif123 <154259489+Mohameddaif123@users.noreply.github.com>
2024-06-02 13:10:42 +0300

    Update productsAPI.js

--- src/components/PRODUCTS/productsAPI.js
+++ src/components/PRODUCTS/productsAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const MY_SERVER = "http://127.0.0.1:8000/products";
+const MY_SERVER = "https://cats-dogs-petstore.onrender.com/products";
 
 export function getProds() {
     return axios.get(MY_SERVER);
