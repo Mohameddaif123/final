c86086abd93daf8b64dcfcb8131d3d07a6c8a768
Mohameddaif123 <154259489+Mohameddaif123@users.noreply.github.com>
2024-06-02 14:24:18 +0300

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,7 +3,6 @@ import { useDispatch } from 'react-redux';
 import { registerAsync } from './loginSlice';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import AdSense from './AdSense';
 import {
     Box,
     Button,
