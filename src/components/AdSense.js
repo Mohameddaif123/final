e7b0a0499ba199bd781773136566821e376986d2
Mohameddaif123 <154259489+Mohameddaif123@users.noreply.github.com>
2024-06-02 13:55:11 +0300

    Update AdSense.js

--- src/components/AdSense.js
+++ src/components/AdSense.js
@@ -1,10 +1,8 @@
-// AdSense.js
-
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 
-const AdSense = ({ client, slot }) => {
+const AdSense = ({ client }) => {
   useEffect(() => {
     (window.adsbygoogle = window.adsbygoogle || []).push({});
   }, []);
@@ -19,8 +17,8 @@ const AdSense = ({ client, slot }) => {
       <ins
         className="adsbygoogle"
         style={{ display: 'block' }}
-        data-ad-client={ca-pub-4481504048762478}
-        data-ad-slot={3937695569}
+        data-ad-client="ca-pub-4481504048762478"// Use the client prop here
+        data-ad-slot="3937695569" // Replace with your AdSense slot ID
         data-ad-format="auto"
         data-full-width-responsive="true"
       ></ins>
@@ -30,7 +28,6 @@ const AdSense = ({ client, slot }) => {
 
 AdSense.propTypes = {
   client: PropTypes.string.isRequired,
-  slot: PropTypes.string.isRequired,
 };
 
 export default AdSense;
